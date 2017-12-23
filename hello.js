35e4b39252897b0c9ecbb126f9f3b88e09f3a83d
Zydan1 <hamzazydan5@gmail.com>
2017-12-23 01:15:07 +0100

    homework javascript week1

---
+++ hello.js
@@ -0,0 +1,124 @@
+'use strict'
+// exercise 1 
+console.log('-------------exercise 1----------------')
+
+console.log('MaRHBA  Donia ', 'Arabic');
+console.log('ciao  mondo ', 'italian');
+console.log('Hallo  wereld ', 'Nederlands');
+console.log('Hello  worold ', 'English');
+
+// exercise 2
+console.log('-------------exercise 2----------------')
+
+console.log('I m Awesome');
+// exercise 3
+console.log('-------------exercise 3----------------')
+
+let x ; 
+console.log('x will be undefined');
+console.log(x);
+x = 123 ;
+console.log('x will be 123');
+console.log(x);
+// exercise 4
+console.log('-------------exercise 4----------------')
+
+let y = 'I am a variable';
+console.log('y will be  am a variable ');
+
+console.log(y);
+// exercise 5
+console.log('-------------exercise 5----------------')
+
+const z = 7.25 ;
+console.log(z) ;
+const a = Math.round(z);
+console.log(a) ;
+if (z > a){console.log(z);} 
+else{console.log(a);}
+// exercise 6 
+console.log('-------------exercise 6----------------')
+
+let pets = [];
+console.log('the value of variable will be [Nothing in this aaray]');
+console.log(pets);
+pets = ['cat','sheep','hamster','chicken','horse'];
+console.log(pets);
+pets.push('Dog');
+console.log(pets);
+// exercise 7
+console.log('-------------exercise 7----------------')
+
+let myString = 'this is a test string for calculating it s length!';
+console.log(myString);
+console.log(myString.length);
+// exercise 8
+console.log('-------------exercise 8----------------')
+
+let x8 = 9;
+console.log('the value of x8:' + x8 );
+let y8 = 'hello';
+console.log('the value of y8 : ' + y8);
+let z8 = 3.42;
+console.log('the value of z8 : ' + z8);
+let k8 = [32 , 'age'];
+console.log('the value of k8 : ' + k8);
+console.log('the value of x8:' + x8  + typeof(x8));
+console.log('the value of y8 : ' + y8 + typeof(y8)) ;
+console.log('the value of z8 : ' + z8 + typeof(z8)) ;
+console.log('the value of k8 : ' + k8 + typeof(k8));
+if(typeof x8 == typeof y8){console.log(x8 + ' and '  + y8  + ' have same type.')}
+else{console.log(x8 + ' and '  + y8  + ' dont have same type.')}
+if(typeof x8 == typeof z8){console.log(x8 + ' and '  + z8  + ' have same type.')}
+else{console.log(x8 + ' and '  + z8  + ' dont have same type.')}
+if(typeof x8 == typeof k8){console.log(x8 + ' and '  + k8  + ' have same type.')}
+else{console.log(x8 + ' and '  + k8  + ' dont have same type.')}
+if(typeof y8 == typeof z8){console.log(y8 + ' and '  + z8  + ' have same type.')}
+else{console.log(y8 + ' and '  + z8  + ' dont have same type.')}
+if(typeof y8 == typeof k8){console.log(y8 + ' and '  + k8  + ' have same type.')}
+else{console.log(y8 + ' and  '  + k8  + ' dont have same type.')}
+if(typeof z8 == typeof k8){console.log(z8 + ' and '  + k8  + ' have same type.')}
+else{console.log(z8 + ' and '  + k8  + ' dont have same type.')}
+// exercise 9
+console.log('-------------exercise 9----------------')
+var num = 6 ;
+var num = num % 3 ;
+console.log("6 % 3 = 0 " );
+console.log('% is the  reminder result of dividing tow  numbers');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
